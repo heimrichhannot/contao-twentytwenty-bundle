482491c2ab3a0cd059d67f75501f1c1e2f72eda7
LongDuc89 <herrfurth.duc.long@gmail.com>
2025-03-25 15:37:48 +0100

    Ordne

--- src/DependencyInjection/HeimrichHannotTwentyTwentyExtension.php
+++ src/DependencyInjection/HeimrichHannotTwentyTwentyExtension.php
@@ -21,7 +21,7 @@ class HeimrichHannotTwentyTwentyExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/config'));
         
         $loader->load('services.yml');
     }
