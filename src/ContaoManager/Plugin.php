820c7f6a6b51c312e24c57c1ece87c7c8a094c6c
LongDuc89 <herrfurth.duc.long@gmail.com>
2025-04-01 14:57:36 +0200

    composer changes

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -13,33 +13,18 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\ManagerPlugin\Config\ContainerBuilder;
-use Contao\ManagerPlugin\Config\ExtensionPluginInterface;
-use HeimrichHannot\BegBundle\HeimrichHannotBegBundle;
 use HeimrichHannot\TwentyTwentyBundle\HeimrichHannotTwentyTwentyBundle;
-use HeimrichHannot\UtilsBundle\Container\ContainerUtil;
 
-class Plugin implements BundlePluginInterface, ExtensionPluginInterface
+class Plugin implements BundlePluginInterface
 {
     /**
      * {@inheritdoc}
      */
-    public function getBundles(ParserInterface $parser)
+    public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(HeimrichHannotTwentyTwentyBundle::class)->setLoadAfter([ContaoCoreBundle::class,HeimrichHannotBegBundle::class])
+            BundleConfig::create(HeimrichHannotTwentyTwentyBundle::class)->setLoadAfter([ContaoCoreBundle::class])
         ];
     }
-    
-    /**
-     * {@inheritdoc}
-     */
-    public function getExtensionConfig($extensionName, array $extensionConfigs, ContainerBuilder $container) {
-        return ContainerUtil::mergeConfigFile(
-            'huh_encore',
-            $extensionName,
-            $extensionConfigs,
-            $container->getParameter('kernel.project_dir').'/vendor/heimrichhannot/contao-twentytwenty-bundle/src/Resources/config/config_encore.yml'
-        );
-    }
+
 }
