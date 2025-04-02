b726cd37b9f30e284ea70962b23ce6e9a5f4182a
LongDuc89 <herrfurth.duc.long@gmail.com>
2025-04-02 11:06:40 +0200

    FrontendAsset use

--- src/Asset/FrontendAsset.php
+++ src/Asset/FrontendAsset.php
@@ -15,7 +15,7 @@ namespace HeimrichHannot\TwentyTwentyBundle\Asset;
 #use HeimrichHannot\UtilsBundle\Container\ContainerUtil;
 #use HeimrichHannot\UtilsBundle\Util\ContainerUtil;
 
-use Heimrichannot\UtilsBundle\StaticUtils\SUtils;
+use HeimrichHannot\UtilsBundle\StaticUtils\SUtils;
 
 class FrontendAsset
 {
