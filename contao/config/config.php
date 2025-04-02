55572f5963c4cbc134821c24117bf12401fc0d78
LongDuc89 <herrfurth.duc.long@gmail.com>
2025-04-02 13:48:28 +0200

    FrontendAsset use

--- contao/config/config.php
+++ contao/config/config.php
@@ -6,7 +6,13 @@
  * Time: 17:44
  */
 
+file_put_contents(
+    __DIR__ . '/../../../../config_debug.log',
+    "config.php wurde geladen\n",
+    FILE_APPEND
+);
+
+
 array_insert($GLOBALS['TL_CTE']['media'], count($GLOBALS['TL_CTE']['media']), [
     'twentytwenty' => \HeimrichHannot\TwentyTwentyBundle\ContentElement\ContentTwentyTwenty::class
 ]);
-file_put_contents(TL_ROOT . '/system/logs/debug.log', print_r($GLOBALS['TL_CTE'], true));
